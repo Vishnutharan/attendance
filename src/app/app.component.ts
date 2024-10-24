fb56d82e506fc3b98904422542580c582744cf58
Vishnutharan <karunakaran.vishnu6@gmail.com>
2024-10-24 21:16:13 +0530

    Secand-Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationComponent, HttpClientModule,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'attendance';
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,37 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig, provideZoneChangeDetection, inject } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { routes } from './app.routes';
+import { AuthService } from './services/auth.service';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        (req, next) => {
+          const authService = inject(AuthService);
+          const token = authService.getToken();
+          
+          if (token) {
+            const authReq = req.clone({
+              headers: req.headers.set('Authorization', `Bearer ${token}`)
+            });
+            return next(authReq);
+          }
+          return next(req);
+        }
+      ])
+    ),
+    provideRouter(
+      routes,
+      withViewTransitions()
+    ),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideZoneChangeDetection({ eventCoalescing: true })
+  ]
 };
